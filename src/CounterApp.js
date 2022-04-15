20094fc7fcc7265d221c9881d5224a590d1bfeba
mcartagena <marcelocartagenaorellana@gmail.com>
2022-04-15 19:12:04 -0400

    Working with react hook useState and button events.

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -1,14 +1,31 @@
 /* react snippet rscp create react stateless component with prototype */
 
-import React from "react";
+import React, {useState} from "react";
 import PropTypes from "prop-types";
 
-const CounterApp = ({ value }) => {
+const CounterApp = ({value = 10}) => {
+
+    const [counter, setCounter] = useState(value);
+
+    const handleEvent = (event) => {
+        setCounter(counter + 1);
+        //setCounter((counter) => counter + 1);
+    };
+    const handleReset = (event) => {
+        setCounter(value);
+    };
+    const handleSubstract = (event) => {
+        setCounter(counter - 1);
+    };
 
     return (
         <>
             <h1>CounterApp</h1>
-            <h2> { value } </h2>
+            <h2> {counter} </h2>
+
+            <button onClick={handleEvent}>+1</button>
+            <button onClick={handleReset}>Reset</button>
+            <button onClick={handleSubstract}>-1</button>
         </>
     );
 
