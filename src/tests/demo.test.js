9148ff543724f37ac1dac3edffbbda767a86e881
mcartagena <marcelocartagenaorellana@gmail.com>
2022-04-20 13:38:38 +0000

    my first test structured

--- src/tests/demo.test.js
+++ src/tests/demo.test.js
@@ -1,6 +1,15 @@
-test('deberia ser verdad', () => {
-    const isActive = true;
-    if(isActive) {
-        throw new Error('deberia estar activo');
-    }
+describe('Pruebas en el archivo demo.test.js', () => {
+    test('deberia ser verdad', () => {
+    
+        // 1.- Inicializacion
+        const mensaje = 'Hola Mundo';
+
+        // 2.- Estimulo
+        const mensaje1 = `Hola Mundo`;
+
+        // 3.- Observar el comportamiento
+        expect(mensaje).toBe(mensaje1);
+
+    })
 })
+
