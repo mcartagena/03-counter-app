9ba6b244377a5e3bf36c8b54611d7bdd96b8a2e4
mcartagena <marcelocartagenaorellana@gmail.com>
2022-04-25 09:51:25 -0400

    Testing promesas

--- src/base/02-template-string.js
+++ src/base/02-template-string.js
@@ -5,6 +5,6 @@ const apellido = 'Herrera';
 // const nombreCompleto = nombre + ' ' + apellido;
 const nombreCompleto = `${ nombre } ${ apellido }`;
 
-export function getSaludo(nombre) {
+export function getSaludo(nombre = 'default') {
     return 'Hola ' + nombre;
 }
